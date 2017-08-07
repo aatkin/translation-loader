5ea13a26d0d563614dedb01cd31c10901e907901
FI\kinnuans <anssi.kinnunen@affecto.com>
2017-08-07 18:56:07 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # translation-loader
-Webpack loader for flattening nested translation files
+Webpack loader for flattening nested translation files, imports JSON and exports JSON
+
+```before.json
+{
+    "app": {
+        "links": {
+            "home": "Back to homepage"
+        },
+        "user": "Current user"        
+    }
+}```
+
+```after.json
+{
+    "app.links.home": "Back to homepage",
+    "app.user": "Current user"
+}```
---
+++ index.js
@@ -0,0 +1,28 @@
+// flattens a nested translation file into more usable format
+// author Anssi Kinnunen
+module.exports = function(source) {
+    // parse source string into manipulable JSON object
+    const json = JSON.parse(source);
+    const flattened = {};
+    const enumerate = (obj, path = "") =>
+    {
+        for (let prop in obj)
+        {
+            let val = obj[prop];
+            if (typeof val !== "object")
+            {
+                flattened[path + prop] = val;
+            }
+            else
+            {
+                const vals = enumerate(val, prop + ".");
+                Object.assign(flattened, vals);
+            }
+        }
+    }
+    enumerate(json);
+
+    // json-loader eats json objects as well
+    const jsonLoader = require("json-loader");
+    return jsonLoader.call(this, flattened);
+};
